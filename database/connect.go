e7f2750417f71d524f74b6ef02e98c729248477c
1ChaLLengeR1 <92648464+1ChaLLengeR1@users.noreply.github.com>
2024-03-06 22:13:13 +0100

    main

---
+++ database/connect.go
@@ -0,0 +1,34 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+)
+
+
+func ConnectToDataBase() {
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_DBNAME")
+
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal("Error to connect  in the Database: ", err)
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Error to pinging in the Database: ", err)
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+}
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"internal/database"
+	"internal/initializers"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+)
+
+type Collection struct {
+	Data []OneResult `json:"data"`
+}
+
+type OneResult struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+	Age  string `json:"age"`
+}
+
+
+var collection = Collection{
+	Data: []OneResult{
+		{Id:"1", Name: "artek", Age: "24"},
+		{Id:"2", Name: "maks", Age: "24"},
+	},
+}
+
+
+func FetchCollection(context *gin.Context){
+	context.IndentedJSON(http.StatusOK, collection)
+}
+
+
+func init(){
+	initializers.LoadEnv()
+	database.ConnectToDataBase()
+}
+
+
+func main(){
+
+	router := gin.Default()
+	router.GET("/collection", FetchCollection)
+	router.Run("localhost:8080")
+
+}
