092960b2465bcca53138d5ab0e4afbc742342493
1ChaLLengeR1 <92648464+1ChaLLengeR1@users.noreply.github.com>
2024-03-06 22:16:53 +0100

    master start

--- main.go
+++ main.go
@@ -3,34 +3,11 @@ package main
 import (
 	"internal/database"
 	"internal/initializers"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
-	_ "github.com/lib/pq"
 )
 
-type Collection struct {
-	Data []OneResult `json:"data"`
-}
 
-type OneResult struct {
-	Id   string `json:"id"`
-	Name string `json:"name"`
-	Age  string `json:"age"`
-}
-
-
-var collection = Collection{
-	Data: []OneResult{
-		{Id:"1", Name: "artek", Age: "24"},
-		{Id:"2", Name: "maks", Age: "24"},
-	},
-}
-
-
-func FetchCollection(context *gin.Context){
-	context.IndentedJSON(http.StatusOK, collection)
-}
 
 
 func init(){
@@ -42,7 +19,5 @@ func init(){
 func main(){
 
 	router := gin.Default()
-	router.GET("/collection", FetchCollection)
 	router.Run("localhost:8080")
-
 }
