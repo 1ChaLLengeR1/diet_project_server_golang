8dcd76d6ddfa7b3c49d12137953435853dcc4d3c
1ChaLLengeR1 <92648464+1ChaLLengeR1@users.noreply.github.com>
2024-05-03 11:48:35 +0200

    new function + off auth0 signUp function in dashboard

--- consumer/handler/auth/auth.go
+++ consumer/handler/auth/auth.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	user_data "internal/consumer/data/user"
@@ -14,85 +15,88 @@ type Auth struct{}
 
 
 func (p *Auth) Authorization(c *gin.Context) {
-	// tokenString := c.GetHeader("Authorization")
 	userData := c.GetHeader("UserData")
-	// err := createUser(c)
-	// if err != nil{
-	// 	c.JSON(http.StatusBadRequest, gin.H{"error: ": err})
-	// 	return
-	// }
-
-	err := checkUser(userData)
+	
+	value, err := checkUser(userData)
 	if err !=nil{
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+
+	if value{
+		value, err := createUser(userData)
+		if err != nil{
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		fmt.Println(value)
+	}
 	
-	c.JSON(http.StatusOK, gin.H{"userData": userData})
+	c.JSON(http.StatusOK, gin.H{"success": userData})
 }
 
-func checkUser(userData string)error{
+func checkUser(userData string)(bool, error){
 	var data user_data.UserData
 	var user user_data.User
 
 	db, err := database.ConnectToDataBase()
 	if err != nil{
-		return err
+		return false, err
 	}
 
 	err = json.Unmarshal([]byte(userData), &data)
 	if err != nil {
-		return fmt.Errorf("error josn userData: %v", err)
+		return false, fmt.Errorf("error josn userData: %v", err)
 	}
 
-	query := `SELECT * FROM users WHERE "userName" = $1 AND "nickName" = $2`
-
-	var users []user_data.User
 	
+	query := `SELECT * FROM users WHERE "email" = $1 AND "nickName" = $2`
+
 	row := db.QueryRow(query, data.Name, data.Nickname)
 	err = row.Scan(&user.Id, &user.UserName, &user.LastName, &user.NickName, &user.Email, &user.Role)
-
-	users = append(users, user)
 	defer db.Close()
 
-	if err != nil{
-		return fmt.Errorf("error queryRow: %v", err)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return true, nil
+		}
+		return false, fmt.Errorf("error scanning row: %v", err)
 	}
-	fmt.Println(users)
 	
-	return nil
+	return false, nil
 }
 
 
-func createUser(c *gin.Context) error{
+func createUser(userData string) ([]user_data.User, error){
 	
 	db, err := database.ConnectToDataBase()
 	if err != nil{
-		return err
+		return nil, err
+	}
+
+	var data user_data.UserData
+	err = json.Unmarshal([]byte(userData), &data)
+	if err != nil {
+		return nil, fmt.Errorf("error josn userData: %v", err)
 	}
 
 	var users []user_data.User
-
-
-	query := `SELECT * FROM users;`
-	rows, err := db.Query(query)
+	query := `INSERT INTO users ("userName", "lastName", "nickName", "email", "role") VALUES ($1, $2, $3, $4, $5);`
+	rows, err := db.Query(query, "", "",  data.Nickname, data.Name, "user")
 	if err != nil{
-		return fmt.Errorf("error db.query: %v", err)
+		return nil, fmt.Errorf("error db.query: %v", err)
 	}
 	defer rows.Close()
 
 	for  rows.Next(){
 		var user user_data.User
 		if err := rows.Scan(&user.Id, &user.UserName, &user.LastName, &user.NickName, &user.Email, &user.Role); err != nil {
-			return fmt.Errorf("error rows.scan: %v", err)
+			return nil, fmt.Errorf("error rows.scan: %v", err)
 		}
 		users = append(users, user)
 	}
 
-	fmt.Println(users)
-	defer db.Close()
-	return nil
-
+	return users, nil
 }
 
 
