c0c3236f52845ea70234ebe1d1bc0263040a13be
1ChaLLengeR1 <92648464+1ChaLLengeR1@users.noreply.github.com>
2024-05-22 12:46:55 +0200

    "Refactor project creation: remove columns from project table, add new handler and create function with database query"

--- consumer/database/migration/05_project.up.sql
+++ consumer/database/migration/05_project.up.sql
@@ -1,11 +1,8 @@
 CREATE TABLE IF NOT EXISTS public.project(
     id UUID NOT NULL DEFAULT uuid_generate_v4(),
     "userId" UUID,
-    "projectId" UUID,
     title VARCHAR COLLATE pg_catalog."default",
     description VARCHAR COLLATE pg_catalog."default",
-    path VARCHAR COLLATE pg_catalog."default",
-    url VARCHAR COLLATE pg_catalog."default",
     "createdUp" date,
     "updateUp" date,
     CONSTRAINT project_pkey PRIMARY KEY (id)
--- consumer/handler/project/create.go
+++ consumer/handler/project/create.go
@@ -1,11 +1,107 @@
 package project
 
 import (
+	params_data "myInternal/consumer/data"
+	project_data "myInternal/consumer/data/project"
+	user_data "myInternal/consumer/data/user"
+	database "myInternal/consumer/database"
+	"myInternal/consumer/handler/auth"
+	helpers "myInternal/consumer/helper"
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
+type ResponseCreateProject struct {
+	Collection []project_data.Create 	`json:"collection"`
+	Status     int 						`json:"status"`
+	Error      string 					`json:"error"`
+}
 
 
-func Create(c *gin.Context) {
-	
+func HandlerCreateProject(c *gin.Context){
+	var createProject project_data.Create
+	c.BindJSON(&createProject)
+
+	jsonMap, err := helpers.BindJSONToMap(&createProject)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, ResponseCreateProject{
+			Collection: nil,
+			Status: http.StatusBadRequest,
+			Error: err.Error(),
+		})
+		return
+	}
+
+	params := params_data.Params{
+		Header: c.GetHeader("UserData"),
+		Param: c.Param("projectId"),
+		Json: jsonMap,
+	}
+
+	project, err := CreateProject(params)
+	if err != nil{
+		c.JSON(http.StatusBadRequest, ResponseCreateProject{
+			Collection: nil,
+			Status: http.StatusBadRequest,
+			Error: err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, ResponseCreateProject{
+		Collection: project.Collection,
+		Status: project.Status,
+		Error: project.Error,
+	})
+
+
+}
+
+
+func CreateProject(params params_data.Params)(ResponseCreateProject, error) {
+	userData := params.Header
+	var usersData []user_data.User
+	var projectsData []project_data.Create
+
+	db, err := database.ConnectToDataBase()
+	if err != nil{
+		return ResponseCreateProject{}, err
+	}
+
+	_, users,  err := auth.CheckUser(userData)
+	if err != nil{
+		return ResponseCreateProject{}, err
+	}
+
+	usersData = users
+
+	title := params.Json["title"]
+	description := params.Json["description"]
+	now := time.Now()
+    formattedDate := now.Format("2006-01-02 15:04:05")
+
+	query := `INSERT INTO project ("userId", title, description, "createdUp", "updateUp") VALUES ($1, $2, $3, $4, $5) RETURNING "id", "userId", "title", "description", "createdUp", "updateUp";`
+
+	rows, err := db.Query(query, usersData[0].Id, title, description, formattedDate, formattedDate)
+    if err != nil {
+        return ResponseCreateProject{}, err
+    }
+	defer rows.Close()
+
+	for rows.Next() {
+		var project project_data.Create
+		if err := rows.Scan(&project.Id, &project.UserId, &project.Title, &project.Description, &project.CreatedUp, &project.UpdateUp); err != nil {
+			return ResponseCreateProject{}, err
+		}
+		projectsData = append(projectsData, project)
+	}
+
+	return ResponseCreateProject{
+		Collection: projectsData,
+		Status: http.StatusOK,
+		Error: "",
+	}, nil
+
 }
