4243aae0ab84cfdb2e226c0cb1b9bfcd0b52866e
1ChaLLengeR1 <92648464+1ChaLLengeR1@users.noreply.github.com>
2024-05-25 12:51:56 +0200

    "Added language support to project creation, updated test cases and database migrations accordingly"

--- consumer/handler/project/create.go
+++ consumer/handler/project/create.go
@@ -1,6 +1,7 @@
 package project
 
 import (
+	"fmt"
 	params_data "myInternal/consumer/data"
 	project_data "myInternal/consumer/data/project"
 	user_data "myInternal/consumer/data/user"
@@ -37,6 +38,7 @@ func HandlerCreateProject(c *gin.Context){
 	params := params_data.Params{
 		Header: c.GetHeader("UserData"),
 		Param: c.Param("projectId"),
+		AppLanguage: c.GetHeader("AppLanguage"),
 		Json: jsonMap,
 	}
 
@@ -55,15 +57,18 @@ func HandlerCreateProject(c *gin.Context){
 		Status: project.Status,
 		Error: project.Error,
 	})
-
-
 }
 
 
 func CreateProject(params params_data.Params)(ResponseCreateProject, error) {
 	userData := params.Header
+	appLanguage := params.AppLanguage
 	var usersData []user_data.User
 	var projectsData []project_data.Create
+
+	if appLanguage == ""{
+		return ResponseCreateProject{}, fmt.Errorf("appLanguage is nil or empty!")
+	}
 
 	db, err := database.ConnectToDataBase()
 	if err != nil{
@@ -82,9 +87,30 @@ func CreateProject(params params_data.Params)(ResponseCreateProject, error) {
 	now := time.Now()
     formattedDate := now.Format("2006-01-02 15:04:05")
 
-	query := `INSERT INTO project ("userId", title, description, "createdUp", "updateUp") VALUES ($1, $2, $3, $4, $5) RETURNING "id", "userId", "title", "description", "createdUp", "updateUp";`
+	query := `INSERT INTO project ("userId", "createdUp", "updateUp") VALUES ($1, $2, $3) RETURNING "id", "userId", "createdUp", "updateUp";`
+	rows, err := db.Query(query, usersData[0].Id, formattedDate, formattedDate)
+    if err != nil {
+        return ResponseCreateProject{}, err
+    }
+	defer rows.Close()
 
-	rows, err := db.Query(query, usersData[0].Id, title, description, formattedDate, formattedDate)
+	var project project_data.Create
+	for rows.Next() {
+		if err := rows.Scan(&project.Id, &project.UserId, &project.CreatedUp, &project.UpdateUp); err != nil {
+			return ResponseCreateProject{}, err
+		}
+	}
+
+	query = `INSERT INTO project_multi_language ("idProject", "idLanguage", "title", "description") VALUES($1, $2, $3, $4) RETURNING "id", "idProject", "idLanguage", "title", "description";`
+	rows, err = db.Query(query, &project.Id, appLanguage, title, description)
+    if err != nil {
+        return ResponseCreateProject{}, err
+    }
+	defer rows.Close()
+
+	query = `SELECT p.id, p."userId", pml."idLanguage", pml.title, pml.description, p."createdUp", p."updateUp" FROM project p JOIN 
+    project_multi_language pml ON p.id = pml."idProject" WHERE  p.id = $1 AND pml."idLanguage" = $2;`
+	rows, err = db.Query(query, &project.Id, appLanguage)
     if err != nil {
         return ResponseCreateProject{}, err
     }
@@ -92,7 +118,7 @@ func CreateProject(params params_data.Params)(ResponseCreateProject, error) {
 
 	for rows.Next() {
 		var project project_data.Create
-		if err := rows.Scan(&project.Id, &project.UserId, &project.Title, &project.Description, &project.CreatedUp, &project.UpdateUp); err != nil {
+		if err := rows.Scan(&project.Id, &project.UserId, &project.IdLanguage, &project.Title, &project.Description, &project.CreatedUp, &project.UpdateUp); err != nil {
 			return ResponseCreateProject{}, err
 		}
 		projectsData = append(projectsData, project)
