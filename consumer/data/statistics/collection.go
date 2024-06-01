b3015d55a491abba177edc318599da511833fa49
1ChaLLengeR1 <92648464+1ChaLLengeR1@users.noreply.github.com>
2024-06-01 17:37:49 +0200

    - create struct folder for statistics and test - create struct data for statistics

---
+++ consumer/data/statistics/collection.go
@@ -0,0 +1,16 @@
+package statistics
+
+type Collection struct {
+	ID           string  `json:"id"`
+	ProjectID    string  `json:"projectId"`
+	Day          string  `json:"day"`
+	EndWeight    float64 `json:"endWeight"`
+	DownWeight   float64 `json:"downWeight"`
+	SumKg        float64 `json:"sumKg"`
+	AvgKg        float64 `json:"avgKg"`
+	SumKcal      int     `json:"sumKcal"`
+	TypeTraining string  `json:"typeTraining"`
+	SumTime      string  `json:"sumTime"`
+	CreatedUp    string  `json:"createdUp"`
+	UpdateUp     string  `json:"updateUp"`
+}
